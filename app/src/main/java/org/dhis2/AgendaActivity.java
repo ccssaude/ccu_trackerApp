b43d0b91a96458612610aa6269572d12ddd04b11
ccsaude <agnaldosamuel@ccsaude.org.mz>
2022-08-12 16:12:09 -0400

    updating the webview

--- app/src/main/java/org/dhis2/AgendaActivity.java
+++ app/src/main/java/org/dhis2/AgendaActivity.java
@@ -2,24 +2,51 @@ package org.dhis2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class AgendaActivity extends AppCompatActivity {
     private WebView webView;
+    private ProgressBar mProgressbar;
 
   @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agenda);
-
         webView = (WebView) findViewById(R.id.webview);
+        mProgressbar = findViewById(R.id.progressBar);
+        mProgressbar.setMax(100);
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("https://gregarious-alpaca-c2f3d8.netlify.app/");
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient(){
+
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                mProgressbar.setProgress(newProgress);
+
+            }
+
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                super.onReceivedTitle(view, title);
+            }
+
+            @Override
+            public void onReceivedIcon(WebView view, Bitmap icon) {
+                super.onReceivedIcon(view, icon);
+            }
+        });
+
+
     }
     @Override
     public void onBackPressed() {
